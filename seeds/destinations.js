a7c8fb24d98268a994f7ee7693130e14318c9308
parvathys0311 <parvathys0311@gmail.com>
2020-07-17 10:16:12 -0600

    first commit

---
+++ seeds/destinations.js
@@ -0,0 +1,47 @@
+
+const destinations= [
+  { 
+    id: 411, 
+    alt: 'Destination 1: New York', 
+    src: 'https://picsum.photos/id/411/200/200', 
+    description: 'New York', 
+    url: 'https://unsplash.com/photos/dgE3lWVyDh8' 
+  },
+  { 
+    id: 318, 
+    alt: 'Destination 2 : Paris', 
+    src: 'https://picsum.photos/id/318/200/200', 
+    description: 'Paris', 
+    url: 'https://i.picsum.photos/id/318/3264/2448.jpg?hmac=zkYZ29-Ww_A4O_kZ3gjlpFQuEYELWgeM6aI_CMG01BU' 
+  },
+  { 
+    id: 323, 
+    alt: 'Destination 3 : Indonesia', 
+    src: 'https://picsum.photos/id/323/200/200', 
+    description: 'Indonesia', 
+    url: 'https://i.picsum.photos/id/323/3831/2554.jpg?hmac=ONpiXSk-ALsNy0EJyL9MxFGjaqDHmP7gU17FGgDk-Jw' 
+  },
+  { 
+    id: 327, 
+    alt: 'Destination 4 : Japan', 
+    src: 'https://picsum.photos/id/327/200/200', 
+    description: 'Japan', 
+    url: 'https://i.picsum.photos/id/327/4442/2961.jpg?hmac=NWAIsS-aGuh9RbL1BJp8jEShGdHWf0Hf5qUaE950en0' 
+  },
+  { 
+    id: 392, 
+    alt: 'Destination 5 : San Fransisco', 
+    src: 'https://picsum.photos/id/392/200/200', 
+    description: 'San Fransisco', 
+    url: 'https://i.picsum.photos/id/392/5580/3720.jpg?hmac=SHJx3DtQS1aAPntmf27wp2mQ5FqQHFKh7hai6wdZSrA' 
+  },
+  { 
+    id: 397, 
+    alt: 'Destination 6 : India ', 
+    src: 'https://picsum.photos/id/397/200/200', 
+    description: 'India', 
+    url: 'https://i.picsum.photos/id/397/4475/2984.jpg?hmac=_PEWxhdxVnCU15wD6E-blJDbpMVH17QQVV0JYMZnkjc' 
+  },
+]
+
+module.exports = destinations;
